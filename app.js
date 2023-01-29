2cc94f4105fc8798473a3eb854fa13e15f24e0c9
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-01-29 15:01:04 -0500

    arrows

---
+++ app.js
@@ -0,0 +1,59 @@
+// Addition
+// function add(a, b) {
+//     return a + b;
+//   }
+// const x = 5;
+// const y = 10;
+// console.log(add(x, y));
+
+// Subtraction
+// function subtract(a, b) {
+//     return a - b;
+//   }
+//   const x = 20;
+//   const y = 4;
+//   console.log(subtract(y, x));
+
+// Multiplication
+// function multiply(a, b) {
+//     return a * b;
+//   }
+// const x = 3;
+// const y = 10;
+// console.log(multiply(x, y));
+
+// Division
+// function divide(a, b) {
+//     return a / b;
+//   }
+// const x = 50;
+// const y = 10;
+// console.log(divide(x, y));
+
+// Addition
+// const add = (a, b) => a + b;
+
+// const x = 5;
+// const y = 10;
+// console.log(add(x, y));
+
+// Subtraction
+// const subtract = (a, b) => a - b;
+
+// const x = 20;
+// const y = 4;
+// console.log(subtract(y, x));
+
+// Multiplication
+// const multiply = (a, b) => a * b;
+
+// const x = 3;
+// const y = 10;
+// console.log(multiply(x, y));
+
+// Division
+// const divide = (a, b) => a / b;
+
+// const x = 50;
+// const y = 10;
+// console.log(divide(x, y));
